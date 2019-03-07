3754e12a204880d4689918dfe6027aa085e552f2
vladodio <47479407+vladodio@users.noreply.github.com>
2019-03-06 21:02:19 -0500

    Update main.c

--- 8-stack/src/main.c
+++ 8-stack/src/main.c
@@ -1,1 +1,39 @@
-#import
+#include <kipr/botball.h>
+const int Rmotor = //x;
+const int Lmotor = //x;
+const int power = 800;
+
+const int Rlift = //x;
+const int Llift = //x;
+const int liftPower = //x;
+  
+const int Rline = //x;
+const int Lline = //x;
+const int black = //x;
+const int white = //x;
+
+void forward(time){
+  mav(Rmotor, power);
+  mav(Lmotor, power);
+  msleep(time);
+}
+
+void flip(ticks){
+  int tick = gmpc(Rlift)+ticks;
+  mav(Rlift, liftPower);
+  mav(Llift, liftPower);
+  while(gmpc() < tick){
+    msleep(10);
+  }
+  ao();
+  printf("I have decided I want to die.");
+  return
+}
+
+void follow(){
+  
+  
+}
+int main(){
+  
+}  
