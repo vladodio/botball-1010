9b6905c04a3409e48835bb89d5beb0165b550a2a
vladodio <47479407+vladodio@users.noreply.github.com>
2019-03-16 11:47:59 -0400

    Update main.c

--- 8-stack/src/main.c
+++ 8-stack/src/main.c
@@ -1,39 +1,44 @@
 #include <kipr/botball.h>
+
 const int Rmotor = //x;
 const int Lmotor = //x;
 const int power = 800;
 
-const int Rlift = //x;
-const int Llift = //x;
-const int liftPower = //x;
-  
+const int Rlift = 3;
+const int Llift = 2;
+const int RPower = 280;
+const int LPower = 270;
+const int degTicks = 15;
+
 const int Rline = //x;
 const int Lline = //x;
 const int black = //x;
 const int white = //x;
+const int grey  = //x;
 
-void forward(time){
-  mav(Rmotor, power);
-  mav(Lmotor, power);
-  msleep(time);
-}
-
-void flip(ticks){
-  int tick = gmpc(Rlift)+ticks;
-  mav(Rlift, liftPower);
-  mav(Llift, liftPower);
-  while(gmpc() < tick){
+void flip(int degrees){
+  int ticks = degrees * degTicks;
+  int time = gmpc(Llift)+ticks;
+  mav(Rlift, RPower);
+  mav(Llift, LPower);
+  while(gmpc(Llift) < time){
     msleep(10);
+    //printf("%d", gmpc(Llift));
   }
   ao();
+  msleep(10);
   printf("I have decided I want to die.");
-  return
 }
 
-void follow(){
+void follow(time){
+  int tick = gmpc(Rlift) + time;
+  if(analog() > ){
   
+  }
   
 }
+
 int main(){
-  
+  flip(45);
 }  
+
