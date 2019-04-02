c41d295f065cafb1f5041db74aa570604e8b9b99
vladodio <47479407+vladodio@users.noreply.github.com>
2019-04-01 20:56:50 -0400

    Update main.c

--- 8-stack/src/main.c
+++ 8-stack/src/main.c
@@ -1,7 +1,9 @@
 #include <kipr/botball.h>
 
-const int RMotor = 2;
-const int LMotor = 3;
+const int RMotor = 3;
+const int LMotor = 2;
+const int Rpower = 1500;
+const int Lpower = 1400;
 const int power = 1500;
 const int left = 1;
 const int right = 0;
@@ -22,107 +24,123 @@ const int RLineS = 1;
 const int LLineS = 2;
 
 void timeFollow(double s){
-double t = seconds() + s;
+    double t = seconds() + s;
     while(seconds() < t){
         if((analog(LineS) > grey)){
-            mav(LMotor, 1000);
-            mav(RMotor, 500);
+            mav(RMotor, 1000);
+            mav(LMotor, 500);
             msleep(5);
             continue;
         }
-        
+
         else if ((analog(LineS) < grey)){
-            mav(LMotor, 500);
-            mav(RMotor, 1000);
+            mav(RMotor, 500);
+            mav(LMotor, 1000);
             msleep(5);
             continue;
         }
         else if (RLineS > black){
-            mav(LMotor, 1200);
-            mav(RMotor, 500);
+            mav(RMotor, 1200);
+            mav(LMotor, 500);
             msleep(5);
             continue;
         }    
         else{
-          mav(LMotor, 250);
-          mav(RMotor, 250);
+            mav(RMotor, 250);
+            mav(LMotor, 250);
         }
     }
+    ao();
 }
 
 void turn(int direction, int degree, int speed){
-    if(direction == 1){
-	int ticks = degree * -tickdeg;
-    int d = gmpc(RMotor);
-    mav(LMotor, speed);
-    mav(RMotor, -speed);
-        
-    while (gmpc(RMotor)-d > ticks) { msleep(10); }
-    ao();
-	}
-    
     if(direction == 0){
-	int ticks = degree * -tickdeg;
-    int d = gmpc(LMotor);
-    mav(LMotor, -speed);
-    mav(RMotor, speed);
-    while (gmpc(LMotor)-d > ticks) { msleep(10); }
+        int ticks = degree * -tickdeg;
+        int d = gmpc(RMotor);
+        mav(LMotor, speed);
+        mav(RMotor, -speed);
+
+        while (gmpc(RMotor)-d > ticks) { msleep(10); }
+        ao();
+    }
+
+    if(direction == 1){
+        int ticks = degree * -tickdeg;
+        int d = gmpc(LMotor);
+        mav(LMotor, -speed);
+        mav(RMotor, speed);
+        while (gmpc(LMotor)-d > ticks) { msleep(10); }
         ao();   
-	}
+    }
     return;
 }
 
 void flip(){
-  enable_servos();
-  set_servo_position(lift, up); 
+    enable_servos();
+    set_servo_position(lift, up); 
+    msleep(4000);
 }
 
+void slowline(double seconds ){
+    double i = 0;
+    seconds = seconds/4;
+    while(i < seconds){
+        timeFollow(4);
+        msleep(250);
+        i++;
+        mav(farming, farmPower);
+    }
+}
+
+void forward(int msecs){
+    mav(RMotor, Rpower);
+    mav(LMotor, Lpower);
+    msleep(msecs);
+    ao();
+}
 int main(){
-	enable_servos();
+    enable_servos();
     set_servo_position(lift, down);
     /*
     turn(right, 90, power);
     msleep(5000);
     return(0);
     */
-    turn(right,120,power);
-    mav(RMotor,power);
+        mav(RMotor,power);
     mav(LMotor,power);
+    msleep(200);
+    turn(right,100,power);
+    mav(RMotor,power);
+    mav(LMotor,power-50);
     while(analog(RLineS) < black){ msleep(5); }
     ao();
     msleep(10);
-    mav(RMotor,power);
-    mav(LMotor,power);
-    msleep(2000);
-    while(analog(RLineS) < black){ msleep(5); }
-    ao();
-    turn(left, 90, power);
-    ao();
+        forward(4000);
+        turn(left, 90, power);
+        ao();
+        mav(RMotor, power);
+        mav(LMotor, power);
+    msleep(3000);
+        while(analog(RLineS) < black) {msleep(5); }
+      
+
+        mav(RMotor, -power);
+        mav(LMotor, -power);
+        msleep(350);
+        ao();
+        turn(left, 95, power);
+        ao();
+        mav(farming, farmPower);
+        slowline(28);
+        ao();
+    turn(left,160,power);
     mav(RMotor, power);
     mav(LMotor, power);
-    while(analog(RLineS) < black) {msleep(5); }
-    mav(RMotor, power);
-    mav(LMotor, power);
-    msleep(2000);
-	
-    while(analog(RLineS) < black){ 
-        mav(RMotor,power);
-   	   mav(LMotor,power);
-        msleep(1);
-     
-    }
+    msleep(3500);
     ao();
-    msleep(100);
-    mav(RMotor, -power);
-    mav(LMotor, -power);
-    msleep(350);
-    ao();
-    turn(left, 95, power);
-    ao();
-    mav(farming, farmPower);
-    timeFollow(60);
-    ao();
-    return 0;
-   
-}  
+    turn(left, 180, power);
+    flip();
+        return 0;
+
+        }  
 
