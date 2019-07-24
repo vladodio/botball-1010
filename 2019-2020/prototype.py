ab31cb875eda9c07c2b83319cb92a61e705f8988
vladodio <47479407+vladodio@users.noreply.github.com>
2019-07-24 02:27:33 -0400

    Update prototype.py

--- 2019-2020/prototype.py
+++ 2019-2020/prototype.py
@@ -1,20 +1,16 @@
 # for driving motors and stuff
-#import kipr.shitty.module.py
-from wallaby import *
+from wallaby import * 
 
 # for logging
 from time import time as cTime
+import sys
+
 
 #import threading ??
 # maybe i'll just build the threading into these funtions?
 # good naming convention??
 # moveWithThread?
 #
-
-
-#import configparser
-# Maybe theres a better solution
-# ask in IBC?
 
 
 # To do list:
@@ -41,30 +37,44 @@ from time import time as cTime
 
 
 
-
 class create():
 
-
-	def __init__(self, logger="none"):
+# setup and other core functions
+	def __init__(self, logger):
+		create_connect()
 		enable_servos()
-		if(isinstance(logger, str)):
-			self.loggingEnabled=False
-		else:
-			self.loggingEnabled=True
+		self.log=logger
+		self.log.write("ROOMBA OBJECT CREATED")
 
 	def __del__(self):
-		disable_servos()		 
+		disable_servos()
+		create_stop()
+		create_disconnect()
+		self.log.write("ROOMBA OBJECT DESTROYED")
+
+# note to self cTime returns a time is seconds
+# multiply by 100 for msleep
+	def timer(self, T, func):
+		start = cTime()
+		end = start + T
+		while(end > cTime()):
+			func()
 
 
+# base movement functions
+#figure out which side is left and which is right
 	def move(self, Lpower, Rpower, T):
 		create_drive_direct(Lpower, Rpower)
+		self.log.write("Moving with " + Lpower + " " + Rpower + "for " + T + "ms")
 		msleep(T)
 
 	def stop(self, T):
 		create_drive_direct(0, 0)
+		self.log.write("Stopping for " + T + " ms")
 		msleep(T)
 
 	def turn(self, degree, direction, power=200):
+		self.log.write("Turning to the" + direction + " " + degree +" degrees")
 		if( direction[0].lower() == "r"):
 # fix this
 			method_to_turn_right()
@@ -85,10 +95,10 @@ class create():
         	msleep(1)
 
 
-	def wallfollowR(self, T):
 
 
 
+"""
 
 class legobot():
 
@@ -108,75 +118,49 @@ class legobot():
 	def stop(self, time=.005):
 		mav()
 
-# drones?
 """
-class drone():
-
-
-"""
-
-class config():
-
-        def __init__(self, pathOfConfig="/home/alex/config.ini"):
-                try:
-                        self.config = open(pathOfConfig, "r")
-                except:
-                        print("VALID FILE????")
-
-        def __del__(self):
-                self.config.close()
-
-        def get(self, header, variable, returnType="f"):
-                #ranges on searches are prob off
-                headerFound=False
-                valueLength=0
-                isolatedValue=""
-                found=False
-                try:
-                        for line in self.config:
-                                if(not headerFound and ("[" == line[0]) and (line[1:len(header)+2] == (header + "]"))):
-                                        headerFound=True
-                                        continue
-                                if(headerFound and line[0] == "["):
-                                        break
-                                if(headerFound and (line[:len(variable)+1] == (variable + "="))):
-                                        valueLength=len(line)-len(variable)-1
-                                        print(line[-valueLength:])
-                                        print(valueLength)
-                                        isolatedValue=line[-valueLength:]
-                                        found=True
-                                        break
-                except:
-                        pass
-
-                if(not headerFound):
-                        raise Exception("Header not found.")
-                if(not found):
-                        raise Exception("Variable was not found.")
-
-                if(returnType=="f"):
-                        return(float(isolatedValue))
-                elif(returnType=="i"):
-                        return(int(isolatedValue))
-                elif(returnType=="s"):
-                        return(isolatedValue)
-                else:
-                        return(isolatedValue)
 
 
 class logger():
 
         def __init__(self, folder="/home/alex/"):
-                try:
-                        self.logFile = open(folder+"latest-run.log", "w")
-                except:
-                        raise Exeception("Unable to open Logfile")
-                self.startTime = cTime()
-                self.logFile.write("[ 0.000 ] Start of Log.\n")
+            try:
+                self.logFile = open(folder+"latest-run.log", "w")
+            except:
+                raise Exeception("Unable to open destination folder.")
+            self.startTime = cTime()
+            self.logFile.write("[ 0.000 ] Start of Log.\n")
 
         def __del__(self):
-                self.write("End Of Log.")
-                self.logFile.close
+            self.write("End Of Log.")
+            self.logFile.close
 
         def write(self, info):
-                self.logFile.write( "[ " + ("%.3f" %(cTime() - self.startTime )) + " ] " + info + "\n")
+            self.logFile.write( "[ " + ("%.3f" %(cTime() - self.startTime )) + " ] " + info + "\n")
+
+        def writeNT(self, info):
+        	self.logFile.write(info+ "\n")
+
+"""
+		ok basically decorators are too big brain for me
+		further research required
+        def decorate(func):
+        	def inner():
+        		func()
+        	return inner
+"""
+       	def sensor(self, sensorName, sensorValue):
+       		self.write(sensorName+ "'s current value is " + sensorValue)
+
+        def fatalError(self, error):
+        	self.write("+++ FATAL ERROR +++")
+        	self.write(error)
+        	self.write("+++ END OF REPORT +++")
+
+       	def warning(self, error):
+       		self.write("*** warning ***")
+       		self.write(error)
+       		self.write("*** end of warning ***")
+
+
+
