355d96db3ea280f0e29c548262e005de9796af5f
vladodio <47479407+vladodio@users.noreply.github.com>
2019-07-23 21:57:55 -0400

    Update config.py

--- 2019-2020/config.py
+++ 2019-2020/config.py
@@ -1,7 +1,10 @@
+# Just to be clear 
+# This file has to conform to python syntax
+
 #[Ports]#
-Rmotor=0
-Lmotor=1
+Rmotor = 0
+Lmotor = 1
 
 #[Power]#
-Rpower=600
-Lpower=575
+Rpower = 600
+Lpower = 575
