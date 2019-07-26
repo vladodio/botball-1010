51adca5ac4cd1c89dbd46ec01a1cf632e14e622f
vladodio <47479407+vladodio@users.noreply.github.com>
2019-07-26 11:57:27 -0400

    Update logger.py

--- 2019-2020/logger.py
+++ 2019-2020/logger.py
@@ -1,40 +1,32 @@
 class logger():
 
-		def __init__(self, folder="/home/alex/"):
-			try:
-				self.logFile = open(folder+"latest-run.log", "w")
-			except:
-				raise Exeception("Unable to open destination folder.")
-			self.startTime = cTime()
-			self.logFile.write("[ 0.000 ] Start of Log.\n")
+	def __init__(self, folder="/home/alex/"):
+		try:
+			self.logFile = open(folder+"latest-run.log", "w")
+		except:
+			raise Exeception("Unable to open destination folder.")
+		self.startTime = cTime()
+		self.logFile.write("[ 0.000 ] Start of Log.\n")
 
-		def __del__(self):
-			self.write("End Of Log.")
-			self.logFile.close
+	def __del__(self):
+		self.write("End Of Log.")
+		self.logFile.close
 
-		def write(self, info):
-			self.logFile.write( "[ " + ("%.3f" %(cTime() - self.startTime )) + " ] " + info + "\n")
+	def write(self, info):
+		self.logFile.write( "[ " + ("%.3f" %(cTime() - self.startTime )) + " ] " + info + "\n")
 
-		def writeNT(self, info):
-			self.logFile.write(info+ "\n")
+	def writeNT(self, info):
+		self.logFile.write(info+ "\n")
 
-"""
-		ok basically decorators are too big brain for me
-		further research required
-		def decorate(func):
-			def inner():
-				func()
-			return inner
-"""
-		def sensor(self, sensorName, sensorValue):
-			self.write(sensorName+ "'s current value is " + sensorValue)
+	def sensor(self, sensorName, sensorValue):
+		self.write(sensorName+ "'s current value is " + sensorValue)
 
-		def fatalError(self, error):
-			self.write("+++ FATAL ERROR +++")
-			self.write(error)
-			self.write("+++ END OF REPORT +++")
+	def fatalError(self, error):
+		self.write("+++ FATAL ERROR +++")
+		self.write(error)
+		self.write("+++ END OF REPORT +++")
 
-		def warning(self, error):
-			self.write("*** warning ***")
-			self.write(error)
-			self.write("*** end of warning ***")
+	def warning(self, error):
+		self.write("*** warning ***")
+		self.write(error)
+		self.write("*** end of warning ***")
