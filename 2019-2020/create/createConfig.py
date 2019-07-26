bc8e959729cfa70757fdc1b1768e53f5751dd46d
vladodio <47479407+vladodio@users.noreply.github.com>
2019-07-26 13:56:02 -0400

    Update and rename 2019-2020/config.py to 2019-2020/create/createConfig.py

--- 2019-2020/create/createConfig.py
+++ 2019-2020/create/createConfig.py
@@ -4,21 +4,6 @@
 # Is it worth throwing away readability for the sake of
 # the config auto updating based on a calibrate
 
-# {Legobot} #
-
-# [Ports] #
-Rmotor =
-Lmotor =
-
-# [Power] #
-
-# [Sensors] #
-blackL=
-whiteL=
-greyL=
-
-
-
 # [Roomba] #
 
 # [Ports] #
