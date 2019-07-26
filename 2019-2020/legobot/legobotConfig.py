57448a4d56761226817e81d92f1e725c41c910c3
vladodio <47479407+vladodio@users.noreply.github.com>
2019-07-26 13:59:30 -0400

    Create legobotConfig.py

---
+++ 2019-2020/legobot/legobotConfig.py
@@ -0,0 +1,19 @@
+# {Legobot} #
+#       Just to be clear 
+#       This file has to conform to python syntax
+#
+#       Is it worth throwing away readability for the sake of
+#       the config auto updating based on a calibrate
+
+
+# [Ports] #
+Rmotor =
+Lmotor =
+
+# [Power] #
+
+# [Sensors] #
+blackL=
+whiteL=
+greyL=
+
