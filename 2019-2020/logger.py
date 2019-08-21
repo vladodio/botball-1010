b07b5fa9618bdac3785d682b40061e3dbdaa0d72
vladodio <47479407+vladodio@users.noreply.github.com>
2019-08-21 17:50:28 -0400

    Update logger.py

--- 2019-2020/logger.py
+++ 2019-2020/logger.py
@@ -2,34 +2,34 @@ from time import time as cTime
 from datetime import datetime
 # To do:
 # 	 Properly do log file naming
-#    maybe use datetime (Not that type of datetime you mong)
 
 class logger:
 
 
-	def __init__(self, folder="~/logs/"):
-		currTime=datetime.now().strftime('%d-%H:%M')
-		if(folder[-1] != '/'):
-			folder = folder+"/"
+	def __init__(self, folder=""):
+		currTime=datetime.now().strftime('[%d]%H:%M')
 		try:
-			self.logFile = open(folder+"log-"+currTime+".log", "w")
+			self.logFile = open(folder+currTime+".log", "w")
 		except:
 			raise Exeception("Unable to open destination folder.")
-		self.logFile.write("LOG CREATED")
+		self.logFile.write("LOG CREATED\n")
+		print("LOG CREATED")
 
 	def __del__(self):
-		self.write("End Of Log.")
+		self.logFile.write("End Of Log.")
 		self.logFile.close()
 		
 		
 	def start(self):
 		self.startTime = cTime()
 		self.logFile.write("[ 0.00 ] Clock Started.\n")
+		print("[ 0.00 ] Clock Started.")
 
-# maybe its not worth using the try/catch: efficency
 	def write(self, info):
 		try:
-			self.logFile.write( "[ " + ("%.2f" %(cTime() - self.startTime )) + " ] " + info + "\n")
+			timeStampedInfo= "[ " + ("%.2f" %(cTime() - self.startTime )) + " ] " + info 
+			self.logFile.write( timeStampedInfo + "\n")
+			print(timeStampedInfo)
 		except:
 			self.writeNT(info)
 
