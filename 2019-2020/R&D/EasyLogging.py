9eba9b7fd18238fdd8bea0529acdbf1674bd88ec
vladodio <vladodio@yahoo.com>
2019-10-30 20:26:01 -0400

    Slightly optimized

--- 2019-2020/R&D/EasyLogging.py
+++ 2019-2020/R&D/EasyLogging.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2.7
-
+#!/usr/bin/python
+import os
+from datetime import datetime
+from time import time
 
 # Timer
-from time import time # alternative to seconds from wallaby package for testing
-
 startTime=0
 def round(num):
 	return(float('%.3f'%(num)))
@@ -19,45 +19,48 @@ def timeStamp():
 	return("[ " +str(getTime())+ " ] ")
 
 
+# timestamp the top of the log
+currDate = datetime.now()
+date =  currDate.strftime("%a %b %d %-I:%M")
+
 
 # Log File
-from os import walk
-logFolder = "./logs/"
-logNamePreface = "logs#"
+_logFolder = "./"
 _fileList = []
-for (dirpath, dirnames, filenames) in walk(logFolder):
+for (dirpath, dirnames, filenames) in os.walk(logFolder): 	
     _fileList.extend(filenames)
-
 _fileCount = len(_fileList)+1
-_logLocation = logFolder + logNamePreface + str(_fileCount)+".log"
+_logName =  "["+str(_fileCount)+"] " + currDate.strftime("%-I:%M %p") + ".log"
+_logLocation = _logFolder + _logName
 _logfile = open(_logLocation, "w+")
-del _fileCount,_logLocation,_fileList
 
 # timestamp the top of the log
-from datetime import datetime
-date = datetime.now()
-date =  date.strftime("%a %b %d %I:%M")
 _logfile.write( date + "\n")
-
+ 	
 # end user functions
+'''
 def log(function):
 	def wrapper(*args, **kwargs):
 		function(args, kwargs)
-		_logfile.write(function+ " is running with args:" + args +"\n")
+		output = str(function) + " is running with args:" + str(args) +"\n"
+		print(output)
+		_logfile.write(output)
+		function(args, kwargs)
+'''
 
 def printf(*args, **kwargs):
-	output = timeStamp()
-	for item in args:
-		output += " " + str(item)
-	print(output)
-	_logfile.write(output +"\n")
+	try:
+		output = timeStamp()
+		for item in args:
+			output += " " + str(item)
+		print(output)
+		_logfile.write(output +"\n")
+	except(NameError):
+		print("setup not complete yet.")
+		output = ""
+		for item in args:
+			output += " " + str(item)
+		print("intended output:" + output)
 
 
-from time import sleep
-startClock()
-sleep(1)
-printf("hi")
-sleep(1)
-printf("hi2", "hello")
-from subprocess import call
-call(["ls","./logs"])
+del _fileCount,_logLocation,_fileList,_logFolder
