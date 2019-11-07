20c41080d8f9c7f8c875af9899078b4eaa95ef0c
vladodio <vladodio@yahoo.com>
2019-11-07 09:29:25 -0500

    More PID testing

--- 2019-2020/R&D/PIDTester.py
+++ 2019-2020/R&D/PIDTester.py
@@ -5,24 +5,14 @@
 #
 #
 #
-from threading import Thread
-from queue import Queue
-import time
+import PIDV1
 
-def thread1(threadname, q):
-    while True:
-        a = q.get()
-        if a is None: return # Poison pill
-        print a
+times = [range(3)]
+times[0] = PIDV1.pid(.1, .1, .1)
+times[1] = PIDV1.pid(.15, .1, .1)
+times[2] = PIDV1.pid(.1, .15, .1)
+times[3] = PIDV1.pid(.1, .1, .15)
 
-def thread2(threadname, q):
-    a = 0
-    for _ in xrange(10):
-        a += 1
-        q.put(a)
-        time.sleep(1)
-    q.put(None) # Poison pill
-
-queue = Queue()
-thread1 = Thread( target=thread1, args=("Thread-1", queue) )
-thread2 = Thread( target=thread2, args=("Thread-2", queue) )
+print("done")
+for item in times:
+	print(item)
--- 2019-2020/R&D/PIDV1.py
+++ 2019-2020/R&D/PIDV1.py
@@ -2,20 +2,45 @@
 #
 
 # imports
-from time import time as currentTime
-
+from time import time
+from time import sleep
 # Constants to fine tune
-Kp = 1 
-Ki = 1
-Kd = 1
+#Kp = 1 
+#Ki = 1
+#Kd = 1
 
-# error from wanted value 
-error = (wantedTemp - temp)
+def pid(Kp,Ki,Kd):
+	startingTemp = 23.0 
+	wantedTemp = 100.0
+	currentTemp = startingTemp
+	startTime = float(int(time()*3)/3)
 
-#
-accumulation_of_error += error * delta_time
-derivative_of_error = (error - last_error) / delta_time
-last_error = error
+	accumulation_of_error = 0
+	last_error = 0
+	output = 0
+	counter = 0
+	delta_time = .2
 
-#
-output = (error * Kp) + (accumulation_of_error * Ki) + (derivative_of_error * Kd)
+	while(True):
+		currentTemp += output
+	# sleep between cycles
+		sleep(delta_time)
+
+	# error from wanted value 
+		error = (wantedTemp - currentTemp)
+
+	#
+		accumulation_of_error += error * delta_time
+		derivative_of_error = (error - last_error) / delta_time
+		last_error = error
+
+	#
+		output = (error * Kp) + (accumulation_of_error * Ki) + (derivative_of_error * Kd)
+
+		if(abs(error) < .5):
+			counter+=1
+		else:
+			counter = 0
+		if(counter > 10):
+			return(float(int(time()*3)/3)-startTime)
+		print("PID output: "+str(output) + "  temp: " + str(currentTemp))
