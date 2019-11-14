997b5c2979ecc5c734355d649ec4996caefbf12e
Sam <samxdu@gmail.com>
2019-11-13 19:21:18 -0500

    function names and constants added

--- Legobot/config.py
+++ Legobot/config.py
@@ -2,7 +2,12 @@
 # legobot variant
 
 # user generated values
+left_motor = 0
+right_motor = 1
+pulley = 0
 
+toph_left = 0
+toph_right = 1
 
 
 # calibrated values
--- Legobot/general.py
+++ Legobot/general.py
@@ -1,5 +1,6 @@
 # general purpose functions for Explorer Post 1010's botball team
 # legobot variant
+from wallaby import *
 
 # general function for moving
 def move(lPower, rPower, time=null):
@@ -12,4 +13,45 @@ def move(lPower, rPower, time=null):
 	except:
 		pass
 
+def turn(deg):
+    pass
 
+def turn_right_wheel(deg):
+    pass
+
+def turn_left_wheel(deg):
+    pass
+
+def lower_pulley():
+    pass
+
+def raise_pulley(stack):
+    pass
+
+def go_to_black():
+    pass
+
+def go_to_white():
+    pass
+
+
+#BEGIN--------------------------
+wait_for_light(2)
+shut_down_in(119)
+
+lower_pulley()
+raise_pulley(1)
+
+turn_left_wheel(90)
+go_to_black()
+go_to_white()
+go_to_black()
+turn(-90)
+go_to_object()
+lower_pulley()
+raise_pulley(2)
+
+move(5)
+go_to_black()
+turn_right_wheel(-90)
+while(not other_sensor)
