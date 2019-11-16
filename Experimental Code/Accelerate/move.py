62aa8daf8252e4686faedce47198038e23649278
vladodio <vladodio@yahoo.com>
2019-11-16 17:13:49 -0500

    Working acceleration function.

--- Experimental Code/Accelerate/move.py
+++ Experimental Code/Accelerate/move.py
@@ -6,20 +6,45 @@ from time import time
 current_left_power = 0
 current_right_power = 0
 
-
+# nicknames for functions
 def sec():
-	return time()
+	return time()*1000 # this returns current time in miliseconds
+def sleep(T):
+	msleep(T) # sleeps for T miliseconds
 
 
-def drive(Lpower, RPower):
-	create_drive_direct(LPower, RPower)
-
-
-def move(LPower, RPower, time, deltaAccel  = 50, deltaDecel = 50, deltaTime = 50):
+def drive(Lpower, Rpower):
 	global current_right_power, current_left_power
-	
-	
-	while((Lpower != current_left_power) and (Rpower != current_right_power)):
-		drive(current_left_power+deltaAccel, current_right_power+deltaAccel)
+	current_right_power = Rpower
+	current_left_power =  Lpower
+	create_drive_direct(Lpower, Rpower)
 
-	drive()
+def accelTo(Lpower, Rpower, numberOfSteps, deltaTime):
+	global current_right_power, current_left_power
+
+	delta_accel_left = (Lpower - current_left_power)/numberOfSteps
+	delta_accel_right = (Rpower - current_right_power)/numberOfSteps
+
+	counter = numberOfSteps
+
+	while(counter > 0):
+		current_left_power += delta_accel_left
+		current_right_power += delta_accel_right
+		drive(int(current_left_power),int(current_right_power))
+		sleep(deltaTime)
+		counter -= 1
+
+
+def move(Lpower, Rpower, time, decelerationOn=True, numberOfSteps= 5, deltaTime = 50):
+	global current_right_power, current_left_power
+
+	startTime = sec()
+	endTime = startTime+time
+	accelTo(Lpower, Rpower, numberOfSteps, deltaTime)
+	if(decelerationOn):
+		endTime -= (numberOfSteps*deltaTime)
+		while(sec() < endTime):
+			sleep(1) # maybe this value needs to be bigger
+		accelTo(0,0,numberOfSteps,deltaTime)
+
+	
