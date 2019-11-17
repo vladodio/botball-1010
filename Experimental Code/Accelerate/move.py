bc7c5c1762041e7fdef6fa5a9cf1da1f583bd537
vladodio <vladodio@yahoo.com>
2019-11-17 12:55:05 -0500

    Exponential Acceleration.

--- Experimental Code/Accelerate/move.py
+++ Experimental Code/Accelerate/move.py
@@ -5,7 +5,8 @@ from time import time
 
 current_left_power = 0
 current_right_power = 0
-
+max_left_power = 500
+max_right_power = 500
 # nicknames for functions
 def sec():
 	return time()*1000 # this returns current time in miliseconds
@@ -33,10 +34,45 @@ def accelTo(Lpower, Rpower, numberOfSteps, deltaTime):
 		drive(int(current_left_power),int(current_right_power))
 		sleep(deltaTime)
 		counter -= 1
+    current_left_power = Lpower
+    current_right_power = Rpower
+    drive(Lpower,Rpower)
 
+
+def accelToExponential(Lpower, Rpower, numberOfSteps, deltaTime):
+    global current_right_power, current_left_power
+
+    delta_left = (Lpower - current_left_power)
+    delta_right = (Rpower - current_right_power)
+
+    base = 15
+    delta_exponent = 1/numberOfSteps
+    left_exponent_term = (base** delta_exponent)
+    right_exponent_term = (base** delta_exponent)
+
+    inital_left_power = current_left_power
+    inital_right_power = current_right_power
+
+    i_have_no_idea_what_to_call_this_left = (delta_left*(left_exponent_term))
+    i_have_no_idea_what_to_call_this_right = (delta_right*(right_exponent_term))
+    counter = 0
+
+    while(counter < numberOfSteps):
+        current_left_power =  inital_left_power + ((i_have_no_idea_what_to_call_this_left*(right_exponent_term**counter))/base)
+        current_right_power = inital_right_power + ((i_have_no_idea_what_to_call_this_right*(left_exponent_term**counter))/base)
+        drive(int(current_left_power),int(current_right_power))
+        sleep(deltaTime)
+        counter += 1
+    current_left_power = Lpower
+    current_right_power = Rpower
+    drive(Lpower,Rpower)
 
 def move(Lpower, Rpower, time, decelerationOn=True, numberOfSteps= 5, deltaTime = 50):
 	global current_right_power, current_left_power
+	if(Lpower>max_left_power):
+		Lpower = max_left_power
+	if(Rpower>max_right_power):
+		Rpower = max_right_power
 
 	startTime = sec()
 	endTime = startTime+time
@@ -46,5 +82,3 @@ def move(Lpower, Rpower, time, decelerationOn=True, numberOfSteps= 5, deltaTime 
 		while(sec() < endTime):
 			sleep(1) # maybe this value needs to be bigger
 		accelTo(0,0,numberOfSteps,deltaTime)
-
-	
