67deed1cc80a5f308c979f7deed1025f9b3369ed
Sam <samxdu@gmail.com>
2020-02-05 18:15:03 -0500

    Updates Sam made.

---
+++ Legobot/config.py
@@ -0,0 +1,18 @@
+# config file for Explorer Post 1010's botball team
+# legobot variant
+
+# user generated values
+left_motor = 0
+right_motor = 1
+pulley = 2
+
+toph_left = 0
+toph_right = 1
+toph_front = 2
+
+claw = 0
+opened = 100
+closed = 2000
+
+# calibrated values
+###
